2e613439f569c272676f29385dff7ebbb7306981
G4MB3SS <razielvalle@gambess.com>
2020-09-12 21:38:40 -0300

    Raziel Valle Miranda: Mejoras antes salida

--- php/form-process.php
+++ php/form-process.php
@@ -4,11 +4,17 @@ ini_set('sendmail_from', "no-responder@aricaguau.cl");
 //Check whether the form has been submitted
 if (isset($_POST["name"], $_POST["email"], $_POST["msg_subject"], $_POST["message"])) {
 
-    $para = "pedidos@aricaguau.cl";
+    $para = "aricaguau.oscar@gmail.com";
+//    $para = "razielvalle@gambess.com";
     $titulo = $_POST['msg_subject'];
     $mensaje = $_POST['message'];
-    $cabeceras = "From: " . $_POST['email'] . "\r\n" .
-            'Reply-To: '. $_POST["email"] . "\r\n" .
+    
+    $cabeceras .= 'To: Pedidos <pedidos@aricaguau.cl>, Oscar <aricaguau.oscar@gmail.com>' . "\r\n";
+    $cabeceras .= 'From: ' . $_POST["name"] . " ". $_POST['email']  . "\r\n";
+    $cabeceras .= 'Bcc: pedidos@aricaguau.cl' . "\r\n";
+    
+    
+    $cabeceras .= 'Reply-To: '. $_POST["email"] . "\r\n" .
             'X-Mailer: PHP/' . phpversion();
     $success = mail($para, $titulo, $mensaje, $cabeceras);
     if (!$success) {
